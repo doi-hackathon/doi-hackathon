11e3f161d22c4ed3672c5415c29f362764981831
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-07-09 14:12:07 -0500

    Added wire-framing for user and admin dashboard and updated wire-framing for event page, user story and features updated

--- src/main/sql/queries.sql
+++ src/main/sql/queries.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS scan_db;
 
-CREATE USER scan_user@localhost IDENTIFIED BY 'codeup';
-GRANT ALL ON scan_db.* TO scan_user@localhost;
+# CREATE USER scan_user@localhost IDENTIFIED BY 'codeup';
+# GRANT ALL ON scan_db.* TO scan_user@localhost;
