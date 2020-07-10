7100ebf07ca84637a4df723c68c22f72ed1a44bd
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-07-10 10:23:20 -0500

    created events and users repos

---
+++ src/main/java/com/scan4kids/project/daos/EventsRepository.java
@@ -0,0 +1,9 @@
+package com.scan4kids.project.daos;
+
+import com.scan4kids.project.models.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventsRepository extends JpaRepository <Event, Long > {
+
+
+}
