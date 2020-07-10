7b9f4bd826331972fe6ebf28961775bd2e708ee1
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-07-10 17:26:55 -0500

    finished user controller

--- src/main/java/com/scan4kids/project/controllers/UserController.java
+++ src/main/java/com/scan4kids/project/controllers/UserController.java
@@ -1,13 +1,18 @@
 package com.scan4kids.project.controllers;
 
 import com.scan4kids.project.daos.UsersRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserController {
 
     private UsersRepository users;
+    private PasswordEncoder passwordEncoder;
 
-
+    public UserController(UsersRepository users, PasswordEncoder passwordEncoder) {
+        this.users = users;
+        this.passwordEncoder = passwordEncoder;
+    }
 
 }
