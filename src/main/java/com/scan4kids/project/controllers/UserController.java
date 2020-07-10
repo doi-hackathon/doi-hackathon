a7454c71f116fef2a9c51c018bfeeda100d67517
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-07-10 17:54:20 -0500

    added get and post

--- src/main/java/com/scan4kids/project/controllers/UserController.java
+++ src/main/java/com/scan4kids/project/controllers/UserController.java
@@ -1,8 +1,13 @@
 package com.scan4kids.project.controllers;
 
 import com.scan4kids.project.daos.UsersRepository;
+import com.scan4kids.project.models.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -13,6 +18,20 @@ public class UserController {
     public UserController(UsersRepository users, PasswordEncoder passwordEncoder) {
         this.users = users;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/sign-up")
+    public String showSignUpForm(Model model){
+        model.addAttribute("user", new User());
+        return "users/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        users.save(user);
+        return "redirect:/login";
     }
 
 }
