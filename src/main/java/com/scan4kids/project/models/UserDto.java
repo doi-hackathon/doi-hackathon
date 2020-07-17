250c556dbfe0a0ca92971c89a3b11cdaa54d75e6
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-07-17 11:38:33 -0500

    dependancy added for validation and UserDto class with getters and setters

---
+++ src/main/java/com/scan4kids/project/models/UserDto.java
@@ -0,0 +1,52 @@
+package com.scan4kids.project.models;
+
+import com.sun.istack.NotNull;
+
+import javax.validation.constraints.NotEmpty;
+
+public class UserDto {
+    @NotNull
+    @NotEmpty
+    private String username;
+
+    @NotNull
+    @NotEmpty
+    private String password;
+    private String matchingPassword;
+
+    @NotNull
+    @NotEmpty
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMatchingPassword() {
+        return matchingPassword;
+    }
+
+    public void setMatchingPassword(String matchingPassword) {
+        this.matchingPassword = matchingPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
