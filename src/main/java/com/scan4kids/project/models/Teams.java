406f14f8267374b892ac619c400c388e7cda5200
Ry Sutton <ryancesutton@gmail.com>
2020-08-16 21:13:23 -0500

    working in teams model

--- src/main/java/com/scan4kids/project/models/Teams.java
+++ src/main/java/com/scan4kids/project/models/Teams.java
@@ -1,12 +1,19 @@
 package com.scan4kids.project.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "teams")
 
 public class Teams {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column (nullable = false)
+    private String name;
+
 
 
 
