bbcdced798522b5769278876dd35e8314a1611af
Ry Sutton <ryancesutton@gmail.com>
2020-08-18 13:07:45 -0500

    complete score and submission models

--- src/main/java/com/scan4kids/project/models/Score.java
+++ src/main/java/com/scan4kids/project/models/Score.java
@@ -1,4 +1,83 @@
 package com.scan4kids.project.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "scores")
 public class Score {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private int score;
+
+    @OneToOne
+    private User judge;
+
+    @ManyToOne
+    @JoinColumn (name = "submission_id")
+    private Submission submission;
+
+    @Column
+    private String comment;
+
+    public Score() {
+    }
+
+    public Score(long id, int score, User judge, Submission submission, String comment) {
+        this.id = id;
+        this.score = score;
+        this.judge = judge;
+        this.submission = submission;
+        this.comment = comment;
+    }
+
+    public Score(int score, User judge, Submission submission, String comment) {
+        this.score = score;
+        this.judge = judge;
+        this.submission = submission;
+        this.comment = comment;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public User getJudge() {
+        return judge;
+    }
+
+    public void setJudge(User judge) {
+        this.judge = judge;
+    }
+
+    public Submission getSubmission() {
+        return submission;
+    }
+
+    public void setSubmission(Submission submission) {
+        this.submission = submission;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
--- src/main/java/com/scan4kids/project/models/Submission.java
+++ src/main/java/com/scan4kids/project/models/Submission.java
@@ -1,4 +1,57 @@
 package com.scan4kids.project.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "submissions")
 public class Submission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne
+    private Team team;
+
+    @Column
+    private String submission_file;
+
+    public Submission() {
+    }
+
+    public Submission(long id, Team team, String submission_file) {
+        this.id = id;
+        this.team = team;
+        this.submission_file = submission_file;
+    }
+
+    public Submission(Team team, String submission_file) {
+        this.team = team;
+        this.submission_file = submission_file;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    public String getSubmission_file() {
+        return submission_file;
+    }
+
+    public void setSubmission_file(String submission_file) {
+        this.submission_file = submission_file;
+    }
+
 }
