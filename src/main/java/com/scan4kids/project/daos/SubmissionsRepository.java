32fe21a019e6769a3d50cf314c69658a3585bbda
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-08-25 13:54:42 -0500

    feat: changed model in jpa for SubmissionsRepo.

--- src/main/java/com/scan4kids/project/daos/SubmissionsRepository.java
+++ src/main/java/com/scan4kids/project/daos/SubmissionsRepository.java
@@ -1,6 +1,7 @@
 package com.scan4kids.project.daos;
 
+import com.scan4kids.project.models.Submission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SubmissionsRepository extends JpaRepository<SubmissionsRepository, Long> {
+public interface SubmissionsRepository extends JpaRepository<Submission, Long> {
 }
