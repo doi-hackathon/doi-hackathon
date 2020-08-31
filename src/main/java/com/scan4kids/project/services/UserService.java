01425a55a8dd0835f0afd68fba8db6f2f8ac2032
Fer <fernandoza@gmail.com>
2020-08-31 09:58:59 -0500

    fix: remove a line from a new temp list and add more hints

--- src/main/java/com/scan4kids/project/services/UserService.java
+++ src/main/java/com/scan4kids/project/services/UserService.java
@@ -1,13 +1,13 @@
 package com.scan4kids.project.services;
 
 import com.scan4kids.project.daos.UsersRepository;
+import com.scan4kids.project.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -18,14 +18,18 @@ public class UserService {
 
     public void importUsersCSV(){
         List<String> lines = null;
+        User importUser = null;
         try {
             lines = Files.readAllLines(Paths.get("data", "sample-users-to-import.csv"));
-            List<String> newList = new ArrayList<>();
 
             for (String line : lines) {
                 System.out.println("line = " + line);
                 //TODO: Now that you can read a file, separate the values by commas and build a new User object with those pieces and save it into the DB.
+//                importUser = new User("fieldX","fieldY",...);
                 //TODO: Consider hashing the passwords, I believe the best approach is to generate random passwords for them and let me change it in their profile page
+//                usersRepository.save(importUser);
+
+
             }
         } catch (IOException e) {
             e.printStackTrace();
